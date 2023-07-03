e007547a3b554d9fbbca37f9bedab35754a1570f
john <john@pop-os.localdomain>
2023-07-03 16:54:13 +0800

    added login UI

--- lib/main.dart
+++ lib/main.dart
@@ -48,9 +48,220 @@ void main() async {
           create: (_) => DatabaseProvider(db),
         ),
       ],
-      child: EmployeeScan(),
+      child: MaterialApp(
+        theme: ThemeData.light(),
+        debugShowCheckedModeBanner: false,
+        routes: {
+          '/': (_) => Login(),
+          '/home': (_) => EmployeeScan(),
+        },
+      ),
     ),
   );
+}
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  bool isSuccess = true;
+  bool isWaiting = false;
+
+  Future<bool> login() async {
+    // Get the username and password from the text fields
+    String username = _usernameController.text;
+    String password = _passwordController.text;
+
+    // Create the request URL
+    Uri url = Uri.parse("https://ojt.infoactiv.org/api/login");
+
+    // Create the headers
+    Map<String, String> headers = {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    };
+
+    // Create the body of the request
+    Map<String, String> body = {
+      "email": username,
+      "password": password,
+    };
+
+    // Make the request
+    http.Response response =
+        await http.post(url, headers: headers, body: jsonEncode(body));
+
+    // Check the status code
+    if (response.statusCode == 200) {
+      // The request was successful, parse the body
+      String body = response.body;
+      var user = jsonDecode(body);
+      return true;
+    } else {
+      // The request failed, print the error
+      // print(response.statusCode);
+      // print(response.body);
+      return false;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var $ScreenWidth = MediaQuery.of(context).size.width;
+    var $ScreenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: Container(
+              color: Colors.blue,
+              height: $ScreenWidth * (30 / 100),
+              child: Image.asset(
+                'assets/placeholder.jpg',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Text(
+            'Login',
+            style: TextStyle(fontWeight: FontWeight.w500, fontSize: 30),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            width: $ScreenWidth * (75 / 100),
+            height: $ScreenHeight * (30 / 100),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: const [
+                  BoxShadow(
+                      color: Colors.black12,
+                      offset: Offset(2.0, 2.0),
+                      blurRadius: 3.0,
+                      spreadRadius: 0.5)
+                ]),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: $ScreenWidth * (70 / 100),
+                  height: $ScreenWidth * (15 / 100),
+                  child: TextField(
+                    controller: _usernameController,
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.person),
+                      border: OutlineInputBorder(),
+                      label: Text('Username'),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                SizedBox(
+                  width: $ScreenWidth * (70 / 100),
+                  height: $ScreenWidth * (15 / 100),
+                  child: TextField(
+                    controller: _passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.lock),
+                      border: OutlineInputBorder(),
+                      label: Text('Password'),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                const Text(
+                  'Errors', //TODO: Replace for errors
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 13,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                if (isSuccess == true)
+                  SizedBox(
+                    width: $ScreenWidth * (50 / 100),
+                    child: 
+                    !isWaiting ?
+                    ElevatedButton(
+                        onPressed: () async {
+                          bool result = await login();
+
+                          if (result) {
+                            Navigator.popAndPushNamed(context, '/home');
+                          } else {
+                            setState(() {
+                              isSuccess = false;
+                            });
+                          }
+                        },
+                        child: const Text(
+                          'Login',
+                          style: TextStyle(fontSize: 18),
+                        )) :
+                      CircularProgressIndicator(color: Colors.blue,),
+
+                  )
+                else if (isSuccess == false)
+                  SizedBox(
+                    width: $ScreenWidth * (50 / 100),
+                    child: Column(
+                      children: [
+                        ElevatedButton(
+                            onPressed: () async {
+                              bool result = await login();
+
+                              if (result) {
+                                Navigator.popAndPushNamed(context, '/home');
+                              } else {
+                                setState(() {
+                                  isSuccess = false;
+                                });
+                              }
+                            },
+                            child: const Text(
+                              'Login',
+                              style: TextStyle(fontSize: 18),
+                            )),
+                        Text(
+                          'Login Failed',
+                          style: TextStyle(
+                            color: Colors.red,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+              ],
+            ),
+          )
+        ],
+      )),
+    );
+  }
 }
 
 class EmployeeScan extends StatefulWidget {
@@ -75,23 +286,17 @@ class _EmployeeScanState extends State<EmployeeScan> {
     }
   }
 
-
-
   @override
   Widget build(BuildContext context) {
     db_provider = Provider.of<DatabaseProvider>(context);
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData.light(),
-      home: FutureBuilder(
+    return Container(
+      child: FutureBuilder(
         future: fetchEmployeeList(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             List<dynamic>? data = snapshot.data;
             for (int i = 0; i < data!.length; i++) {
               try {
-          
                 // Insert data into the database
                 db_provider.insertEmployee(data[i]['id'], data[i]['first_name'],
                     data[i]['last_name'], data[i]['company_id']);
--- scratch.dart
+++ scratch.dart
@@ -1,17 +1,34 @@
-import 'package:http/http.dart' as http;
-import 'dart:io';
+import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
 
 void main() async {
-  final response = await http.get(Uri.parse('http://ojt.infoactiv.org/api/employee'));
+  // Get the bearer token from somewhere
+  String token = "7|W5VhsRJmt7E6FwUi2PANqrZ8iB6EgyoHHjbGEJ8G";
+  print("1");
+  // Create the request URL
+  Uri url = Uri.parse("http://ojt.infoactiv.org/api/employee");
 
-    if (response.statusCode == 200) {
-      print("200");
-      // The request was successful, parse the JSON
-      // return jsonDecode(response.body) as List<dynamic>;
-    } else {
-      print("Error");
-      // The request failed, throw an error
-      // throw Exception('Something went wrong');
-    }
+  // Create the headers
+  Map<String, String> headers = {
+    "Authorization": "Bearer $token",
+    "Accept": "application/json"
+  };
+  print("1");
+  // Make the request
+  http.Response response = await http.get(url, headers: headers);
+  print("1");
+  // Check the status code
+  if (response.statusCode == 200) {
+    // The request was successful, parse the body
+    String body = response.body;
+    var user = jsonDecode(body);
+    print(user);
+  } else {
+    // The request failed, print the error
+    print(response.statusCode);
+    print(response.body);
+  }
 }
